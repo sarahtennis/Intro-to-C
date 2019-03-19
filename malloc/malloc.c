c1f7ba0840756009ba104e88bdbb19d904210363
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-03-19 15:19:32 -0400

    adds string_dup() function

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -12,7 +12,18 @@
 */
 char *string_dup(char *src)
 {
+    int srcLen = strlen(src);
+    char *dup = malloc(sizeof(char) * srcLen);
+    int x;
 
+    for (x = 0; src[x]; x++)
+    {
+        dup[x] = src[x];
+    }
+
+    dup[x] = '\0';
+
+    return dup;
 }
 
 /*
@@ -24,7 +35,6 @@ char *string_dup(char *src)
 */
 void mem_copy(void *dest, const void *src, int n)
 {
-
 }
 
 /*
@@ -40,7 +50,6 @@ void mem_copy(void *dest, const void *src, int n)
 */
 void *resize_memory(void *ptr, int old_size, int new_size)
 {
-
 }
 
 #ifndef TESTING
@@ -51,36 +60,36 @@ int main(void)
 
     printf("Duplicated string: %s\n", dup);
 
-    int numbers[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
-    int n = sizeof(numbers) / sizeof(numbers[0]);
-    int *target = malloc(n * sizeof(int));
-    
-    mem_copy(target, numbers, n * sizeof(int));
+    // int numbers[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
+    // int n = sizeof(numbers) / sizeof(numbers[0]);
+    // int *target = malloc(n * sizeof(int));
 
-    printf("Copied array: ");
+    // mem_copy(target, numbers, n * sizeof(int));
 
-    for (int i = 0; i < n; i++) {
-        printf("%d ", target[i]);
-    }
+    // printf("Copied array: ");
 
-    printf("\n");
+    // for (int i = 0; i < n; i++) {
+    //     printf("%d ", target[i]);
+    // }
 
-    char *url = string_dup("http://lambdaschool.com");
-    char *path = string_dup("/students/");
-    int url_length = string_length(url);
-    int path_length = string_length(path);
-    
-    int new_length = url_length - 1 + path_length;
-    char *new_url = resize_memory(url, url_length, new_length);
-    char *p = new_url + url_length;
+    // printf("\n");
 
-    while (*path != '\0') {
-        *p = *path;
-        p++;
-        path++;
-    }
+    // char *url = string_dup("http://lambdaschool.com");
+    // char *path = string_dup("/students/");
+    // int url_length = string_length(url);
+    // int path_length = string_length(path);
 
-    printf("Full path string: %s\n", new_url);
+    // int new_length = url_length - 1 + path_length;
+    // char *new_url = resize_memory(url, url_length, new_length);
+    // char *p = new_url + url_length;
+
+    // while (*path != '\0') {
+    //     *p = *path;
+    //     p++;
+    //     path++;
+    // }
+
+    // printf("Full path string: %s\n", new_url);
 
     return 0;
 }
